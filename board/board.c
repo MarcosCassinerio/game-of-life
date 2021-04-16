79c32b82755d42c53235197459bf2e21ff56a999
Marcos Cassinerio <mcassinerio@fceia.unr.edu.ar>
2021-04-16 19:15:36 -0300

    Initial commit

---
+++ board/board.c
@@ -0,0 +1,142 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "board.h"
+
+int board_init(board_t *board, size_t col, size_t row) {
+    if (col < 0 || row < 0)
+        return 1;
+
+    int i = 0, falla = 0;
+    
+    board = malloc(sizeof(board_t));
+
+    board->col = col;
+    board->row = row;
+
+    board->cell = malloc(sizeof(char*) * row);
+
+    if (!(board->cell)) {
+        free(board);
+        return 1;
+    }
+    
+    for (; i < row; ++i) {
+        board->cell[i] = malloc(sizeof(char) * col);
+        if (!(board->cell)) {
+            falla = 1;
+            break;
+        }
+    }
+
+    if (falla) {
+        for (; i > 0; --i)
+            free(board->cell[i]);
+        free(board->cell);
+        free(board);
+        return 1;
+    }
+
+    return 0;
+}
+
+int board_init_def(board_t *board, size_t col, size_t row, char def) {
+    if (col < 0 || row < 0)
+        return 1;
+
+    int i = 0, h, falla = 0;
+    
+    board = malloc(sizeof(board_t));
+
+    board->col = col;
+    board->row = row;
+
+    board->cell = malloc(sizeof(char*) * row);
+
+    if (!(board->cell)) {
+        free(board);
+        return 1;
+    }
+    
+    for (; i < row; ++i) {
+        board->cell[i] = malloc(sizeof(char) * col);
+        if (!(board->cell)) {
+            falla = 1;
+            break;
+        }
+        for (h = 0; h < col; ++h)
+            board->cell[i][h] = def;
+    }
+
+    if (falla) {
+        for (; i > 0; --i)
+            free(board->cell[i]);
+        free(board->cell);
+        free(board);
+        return 1;
+    }
+
+    return 0;
+}
+
+char board_get(board_t board, unsigned int col, unsigned int row) {
+    if (col >= board.col || row >= board.row)
+        return NULL;
+
+    return board.cell[row][col];
+}
+
+char board_get_round(board_t board, int col, int row) {
+    row = row % board.row;
+
+    col = col % board.col;
+
+    return board.cell[row >= 0 ? row : (board.row + row)][col >= 0 ? col : (board.col + col)];
+}
+
+int board_set(board_t board, unsigned int col, unsigned int row, char val) {
+    if (col >= board.col || row >= board.row)
+        return 1;
+
+    board.cell[row][col] = val;
+
+    return 0;
+}
+
+int board_load(board_t *board, char *str) {
+    int i = 0, h = 1, j, falla = 0;
+    for (; i < strlen(str); ++i) {
+        if (isdigit(*(str + i)))
+            h = *(str + i) + 0;
+        else if (isalpha(*(str + i))) {
+            for (j = 0; j < h; ++j) {
+                falla = board_set(*board, (i % board->row) % board->col, i % board->row, *(str + i));
+                if (falla)
+                 break;
+            }
+            h = 1;
+        }
+    }
+    return falla;
+}
+
+void board_show(board_t board, char *res) {
+    int i = 0, j;
+    res = malloc(sizeof(char) * board.row * board.col);
+    for (; i < board.row; ++i) {
+        for (j = 0; j < board.col; ++j)
+            *(res + (i * board.row) + j) = board.cell[i][j];
+        *(res + (i * board.row) + j) = '\n';
+    }
+    *(res + (i * board.row) + j) = '\0';
+}
+
+void board_destroy(board_t *board) {
+    int i = 0, j;
+    for (; i < board->row; ++i) {
+        for (j = 0; j < board->col; ++j)
+            free(board->cell[i][j]);
+        free(board->cell[i]);
+    }
+    free(board);
+}
---
+++ board/board.h
@@ -0,0 +1,45 @@
+#ifndef BOARD
+#define BOARD
+
+#include <stdlib.h>
+
+/******************************************************************************/
+/* Definición de la estructura de datos del tablero */
+
+struct _board {
+    char** cell;
+    unsigned int col;
+    unsigned int row;
+};
+typedef struct _board board_t;
+/******************************************************************************/
+/******************************************************************************/
+
+/* Funciones sobre el tablero */
+
+/* Creación del tablero */
+int board_init(board_t *board, size_t col, size_t row);
+
+/* Creación del tablero con un elemento por default*/
+int board_init_def(board_t *board, size_t col, size_t row, char def);
+
+/* Leer el tablero en una posición (col, row) */
+char board_get(board_t board, unsigned int col, unsigned int row);
+
+/* Leer el tablero en una posición asumiendo que el tablero es 'redondo'.*/
+char board_get_round(board_t board, int col, int row);
+
+/* Asignarle un valor 'val' a la posición (col, row) del tablero*/
+int board_set(board_t board, unsigned int col, unsigned int row, char val);
+
+/* Leer de una lista de caracteres que codifican un tablero en formato RLE e
+ * interpretarla como un tablero */
+int board_load(board_t *board, char *str);
+
+/* Función para mostrar el tablero */
+/* La función 'board_show' asume que hay espacio suficiente en 'res' para alojar el tablero.*/
+void board_show(board_t board, char *res);
+
+/* Destroy board */
+void board_destroy(board_t *board);
+#endif
